f0827a24969f73d93ff16aef04753f7542954447
João Paulo Polles <joaopaulopolles@gmail.com>
2019-06-25 11:18:39 -0300

    correções nas migrations

--- database/migrations/2019_04_04_222055_create_software_table.php
+++ database/migrations/2019_04_04_222055_create_software_table.php
@@ -14,6 +14,7 @@ class CreateSoftwareTable extends Migration
     public function up()
     {
         Schema::create('softwares', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->increments('id');
             $table->string('name');
             $table->timestamps();
