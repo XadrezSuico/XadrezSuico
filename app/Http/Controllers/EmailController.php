16be0f77cf955685349c38b18be4d935dc821ac0
João Paulo Polles <joaopaulopolles@gmail.com>
2019-06-26 03:14:32 -0300

    corrigida a falta de importação de alguns componentes

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\EmailSend;
 use App\Email;
 
 class EmailController extends Controller
