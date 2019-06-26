e35bd034734ea9ce27dca3771196d1801bc6299f
João Paulo Polles <joaopaulopolles@gmail.com>
2019-06-26 02:34:56 -0300

    layout de email

---
+++ resources/views/layouts/mail.blade.php
@@ -0,0 +1,10 @@
+<div id="mail_portalinterno">
+	<header style="background: #26704c; width: 100%; text-align: center; padding: 20px;">
+		<h2>Sistema XadrezSuiço</h2>
+	</header>
+	<section style="text-align: center;">
+		{!!$content!!}
+	</section>
+	<footer style="text-align: center;"><hr/>
+	Esta conta de email não é monitorada, portanto, favor não responder esta mensagem.</footer>
+</div>
