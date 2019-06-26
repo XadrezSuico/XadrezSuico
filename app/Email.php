c524d052c81878bcb8a16d1b4fede7ffbedd5d28
João Paulo Polles <joaopaulopolles@gmail.com>
2019-06-26 03:14:16 -0300

    criado a relação com enxadrista

--- app/Email.php
+++ app/Email.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Email extends Model
 {
-    //
+    public function enxadrista(){
+        return $this->belongsTo("App\Enxadrista","enxadrista_id","id");
+    }
 }
