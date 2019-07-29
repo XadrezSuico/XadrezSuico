01e2e42d4e9d7e252287459db2b86c2fbe4f839e
João Paulo Polles <joaopaulopolles@gmail.com>
2019-07-29 17:42:01 -0300

    criado o usa lbx

---
+++ database/migrations/2019_07_29_174028_update_evento_table_add_l_b_x.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateEventoTableAddLBX extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('enxadrista', function (Blueprint $table) {
+            $table->boolean("usa_lbx")->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
