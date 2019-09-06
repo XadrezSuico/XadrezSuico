6f485a0b534b4ccf20c97926cc0e71362b6a4340
João Paulo Polles <joaopaulopolles@gmail.com>
2019-09-06 13:11:46 -0300

    agora tem um botão para baixar a lista de inscritos na tela de lista de inscritos dos torneios

--- resources/views/evento/torneio/inscricao/index.blade.php
+++ resources/views/evento/torneio/inscricao/index.blade.php
@@ -76,10 +76,25 @@
 @endsection
 
 @section("js")
+@foreach(array(
+    "https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js",
+    "https://cdn.datatables.net/buttons/1.5.6/js/buttons.flash.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js",
+    "https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js",
+    "https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js"
+    ) as $url)
+<script type="text/javascript" src="{{$url}}"></script>
+@endforeach
 <script type="text/javascript">
     $(document).ready(function(){
         $("#tabela").DataTable({
             responsive: true,
+            dom: 'Bfrtip',
+            buttons: [
+                'copy', 'csv', 'excel', 'pdf', 'print'
+            ]
         });
     });
 </script>
