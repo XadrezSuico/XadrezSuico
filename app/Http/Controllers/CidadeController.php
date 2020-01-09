cc456aaf9a7c385b8f018c56b5c6c0b91f1f616b
João Paulo Polles <03986557156@AD.MCR.PR.GOV.BR>
2020-01-09 09:17:34 -0300

    agora é controlado também através dos métodos de Cidade e Clube se o usuário tem permissão para acesso

--- app/Http/Controllers/CidadeController.php
+++ app/Http/Controllers/CidadeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Cidade;
 
 class CidadeController extends Controller
@@ -11,29 +12,59 @@ class CidadeController extends Controller
 		return $this->middleware("auth");
 	}
     public function index(){
+		$user = Auth::user();
+		if(!$user->hasPermissionGlobalbyPerfil([1,2,8])){
+			return redirect("/");
+        }
+        
         $cidades = Cidade::all();
         return view('cidade.index',compact("cidades"));
     }
     public function new(){
+		$user = Auth::user();
+		if(!$user->hasPermissionGlobalbyPerfil([1,2,8])){
+			return redirect("/");
+        }
+        
         return view('cidade.new');
     }
     public function new_post(Request $request){
+		$user = Auth::user();
+		if(!$user->hasPermissionGlobalbyPerfil([1,2,8])){
+			return redirect("/");
+        }
+        
         $cidade = new Cidade;
         $cidade->name = $request->input("name");
         $cidade->save();
         return redirect("/cidade/edit/".$cidade->id);
     }
     public function edit($id){
+		$user = Auth::user();
+		if(!$user->hasPermissionGlobalbyPerfil([1,2,8])){
+			return redirect("/");
+        }
+        
         $cidade = Cidade::find($id);
         return view('cidade.edit',compact("cidade"));
     }
     public function edit_post($id,Request $request){
+		$user = Auth::user();
+		if(!$user->hasPermissionGlobalbyPerfil([1,2,8])){
+			return redirect("/");
+        }
+        
         $cidade = Cidade::find($id);
         $cidade->name = $request->input("name");
         $cidade->save();
         return redirect("/cidade/edit/".$cidade->id);
     }
     public function delete($id){
+		$user = Auth::user();
+		if(!$user->hasPermissionGlobalbyPerfil([1,2,8])){
+			return redirect("/");
+        }
+        
         $cidade = Cidade::find($id);
         
         if($cidade->isDeletavel()){
--- app/Http/Controllers/ClubeController.php
+++ app/Http/Controllers/ClubeController.php
@@ -12,14 +12,29 @@ class ClubeController extends Controller
 		return $this->middleware("auth");
 	}
     public function index(){
+		$user = Auth::user();
+		if(!$user->hasPermissionGlobalbyPerfil([1,2,8])){
+			return redirect("/");
+        }
+        
         $clubes = Clube::all();
         return view('clube.index',compact("clubes"));
     }
     public function new(){
+		$user = Auth::user();
+		if(!$user->hasPermissionGlobalbyPerfil([1,2,8])){
+			return redirect("/");
+        }
+        
         $cidades = Cidade::all();
         return view('clube.new',compact("cidades"));
     }
     public function new_post(Request $request){
+		$user = Auth::user();
+		if(!$user->hasPermissionGlobalbyPerfil([1,2,8])){
+			return redirect("/");
+        }
+        
         $clube = new Clube;
         $clube->name = $request->input("name");
         $clube->cidade_id = $request->input("cidade_id");
@@ -27,11 +42,21 @@ class ClubeController extends Controller
         return redirect("/clube/edit/".$clube->id);
     }
     public function edit($id){
+		$user = Auth::user();
+		if(!$user->hasPermissionGlobalbyPerfil([1,2,8])){
+			return redirect("/");
+        }
+        
         $clube = Clube::find($id);
         $cidades = Cidade::all();
         return view('clube.edit',compact("clube","cidades"));
     }
     public function edit_post($id,Request $request){
+		$user = Auth::user();
+		if(!$user->hasPermissionGlobalbyPerfil([1,2,8])){
+			return redirect("/");
+        }
+        
         $clube = Clube::find($id);
         $clube->name = $request->input("name");
         $clube->cidade_id = $request->input("cidade_id");
@@ -39,6 +64,11 @@ class ClubeController extends Controller
         return redirect("/clube/edit/".$clube->id);
     }
     public function delete($id){
+		$user = Auth::user();
+		if(!$user->hasPermissionGlobalbyPerfil([1,2,8])){
+			return redirect("/");
+        }
+        
         $clube = Clube::find($id);
         
         if($clube->isDeletavel()){
