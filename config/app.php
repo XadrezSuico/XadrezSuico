25745f5234d87cb19fc21ac4089019a61e4e58c5
Joao <joaopaulopolles@gmail.com>
2022-06-15 03:40:24 -0300

    dado início a versão 0.2.0.0

--- config/app.php
+++ config/app.php
@@ -25,7 +25,7 @@ return [
     | or any other location as required by the application or its packages.
     */
 
-    'version' => '0.1.0.1 Beta',
+    'version' => '0.2.0.0 Beta',
 
     /*
     |--------------------------------------------------------------------------
