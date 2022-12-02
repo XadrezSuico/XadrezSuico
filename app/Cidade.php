5d4ca136dcd3e3d8979629829f81e28561493805
João Paulo Polles <joaopaulopolles@gmail.com>
2022-12-02 05:27:08 -0300

    correções

--- app/Cidade.php
+++ app/Cidade.php
@@ -25,11 +25,11 @@ class Cidade extends Model
     {
         parent::boot();
 
-        self::creating(function($model){
-            if($model->uuid == NULL){
-                $model->uuid = Str::uuid();
-            }
-        });
+        // self::creating(function($model){
+        //     if($model->uuid == NULL){
+        //         $model->uuid = Str::uuid();
+        //     }
+        // });
 
         // self::created(function($model){
         //     // ... code here
