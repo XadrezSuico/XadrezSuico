cc5008e6931a6bcb956abeae0900698f43c1d35d
João Paulo Polles <joaopaulopolles@gmail.com>
2023-02-12 21:37:48 -0300

    correções de erro

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -15,13 +15,23 @@ class RatingController extends Controller
         return view("rating.index", compact("tipos_rating"));
     }
     function list($tipo_rating_id) {
+        if(TipoRating::where([["id","=",$tipo_rating_id]])->count() == 0){
+            return abort(404);
+        }
         $tipo_rating = TipoRating::find($tipo_rating_id);
         return view("rating.list", compact("tipo_rating"));
     }
     public function view($tipo_rating_id, $rating_id)
     {
+        if(TipoRating::where([["id","=",$tipo_rating_id]])->count() == 0){
+            return abort(404);
+        }
         $tipo_rating = TipoRating::find($tipo_rating_id);
+        if(Rating::where([["id","=",$rating_id]])->count() == 0){
+            return abort(404);
+        }
         $rating = Rating::find($rating_id);
+
         $enxadrista = $rating->enxadrista;
         return view("rating.view", compact("tipo_rating", "rating", "enxadrista"));
     }
