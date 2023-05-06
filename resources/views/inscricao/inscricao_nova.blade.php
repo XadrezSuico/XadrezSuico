e17875d89dcba72ae5c737bb3d6326d9d3f1ba57
João Paulo Polles <joaopaulopolles@gmail.com>
2023-05-06 09:46:32 -0300

    melhorias

--- resources/views/inscricao/inscricao_nova.blade.php
+++ resources/views/inscricao/inscricao_nova.blade.php
@@ -591,7 +591,7 @@
 					<select id="inscricao_clube_id" class="clube_id this_is_select2 form-control">
 						<option value="">--- Você pode escolher um clube/instituição/escola ---</option>
 						@foreach(\App\Clube::all() as $clube)
-							<option value="{{$clube->id}}">{{$clube->cidade->estado->pais->name}}-{{$clube->cidade->estado->name}}/{{$clube->cidade->name}} - {{$clube->name}}</option>
+							<option value="{{$clube->id}}">@if($clube->cidade){{$clube->cidade->estado->pais->name}}-{{$clube->cidade->estado->name}}/{{$clube->cidade->name}} - @endif{{$clube->name}}</option>
 						@endforeach
 					</select>
                     <button id="clubeNaoCadastradoInscricao" onclick="chamaCadastroClube(1)" class="btn btn-success">O meu clube/instituição/escola não está cadastrado</button>
@@ -674,7 +674,7 @@
 					<select id="confirmacao_clube_id" class="clube_id this_is_select2 form-control">
 						<option value="">--- Você pode escolher um clube/instituição/escola ---</option>
 						@foreach(\App\Clube::all() as $clube)
-							<option value="{{$clube->id}}">{{$clube->cidade->estado->pais->name}}-{{$clube->cidade->estado->name}}/{{$clube->cidade->name}} - {{$clube->name}}</option>
+							<option value="{{$clube->id}}">@if($clube->cidade){{$clube->cidade->estado->pais->name}}-{{$clube->cidade->estado->name}}/{{$clube->cidade->name}} - @endif{{$clube->name}}</option>
 						@endforeach
 					</select>
                     <button id="clubeNaoCadastradoInscricao" onclick="chamaCadastroClube(2)" class="btn btn-success">O meu clube/instituição/escola não está cadastrado</button>
