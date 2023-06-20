20a300688dc77594125e84e0c591c27dbd5507ff
João Paulo Polles <joaopaulopolles@gmail.com>
2023-06-19 22:48:09 -0300

    correções

--- app/Http/Controllers/API/Event/PlayerController.php
+++ app/Http/Controllers/API/Event/PlayerController.php
@@ -249,7 +249,7 @@ class PlayerController extends Controller
                                 'email' => 'required|string|email:rfc,dns|max:255',
                             ]);
                             if ($validator->fails()) {
-                                return response()->json(["ok" => 0, "error" => 1, "message" => "O e-mail é inválido. Por favor, verifique e tente novamente.", "registred" => 0, "ask" => 0]);
+                                return response()->json(["ok" => 0, "error" => 1, "message" => "O e-mail é inválido. Por favor, verifique e tente novamente."]);
                             }
                             $enxadrista->email = $request->email;
                         }else{
