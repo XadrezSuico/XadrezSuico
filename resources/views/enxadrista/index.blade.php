03c5daf16f6334fba575fc8b8facca1d59a2c39f
João Paulo Polles <joaopaulopolles@gmail.com>
2023-08-29 23:03:00 -0300

    correção

--- resources/views/enxadrista/index.blade.php
+++ resources/views/enxadrista/index.blade.php
@@ -32,7 +32,7 @@
 
             @if(
                 \Illuminate\Support\Facades\Auth::user()->hasPermissionGlobal() ||
-                \Illuminate\Support\Facades\Auth::user()->hasPermissionGlobalbyPerfil([9]) ||
+                \Illuminate\Support\Facades\Auth::user()->hasPermissionGlobalbyPerfil([9])
             )
                 <li role="presentation"><a href="{{url("/enxadrista/download")}}">Baixar Base de Dados (Apenas Administradores e Super-Administradores)</a></li>
             @endif
