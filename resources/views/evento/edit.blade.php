e5f5852471812349c1fcfa5d735ab9271d3fce7f
João Paulo Polles <joaopaulopolles@gmail.com>
2024-02-26 16:32:06 -0300

    removido bloco

--- resources/views/evento/edit.blade.php
+++ resources/views/evento/edit.blade.php
@@ -477,37 +477,6 @@
                                     </div>
                                 @endif
 
-                                @php($bigger_tournament = $evento->getTournamentWithMoreRegistrations())
-                                    <!-- Maior Torneio -->
-                                    <div class="col-sm-12 col-md-8">
-                                        <!-- small box -->
-                                        <div class="small-box bg-aqua">
-                                            <div class="inner">
-                                            <h3>{{$bigger_tournament["status"] ? $bigger_tournament["tournament"]->name : $bigger_tournament["tournament"]}}</h3>
-
-                                            <p>Maior Torneio</p>
-                                            </div>
-                                            <div class="icon">
-                                            <i class="fa fa-award"></i>
-                                            </div>
-                                            <!--<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>-->
-                                        </div>
-                                    </div>
-                                    <!-- Maior Torneio Total -->
-                                    <div class="col-sm-12 col-md-4">
-                                        <!-- small box -->
-                                        <div class="small-box bg-aqua">
-                                            <div class="inner">
-                                            <h3>{{$bigger_tournament["total"]}}</h3>
-
-                                            <p>Total de Inscritos no Maior Torneio</p>
-                                            </div>
-                                            <div class="icon">
-                                            <i class="fa fa-award"></i>
-                                            </div>
-                                            <!--<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>-->
-                                        </div>
-                                    </div>
 
 
                             </div>
