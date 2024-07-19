f9fa0c6b92a6cbb666de48f0f5ee796ca59565ef
Joao <joaopaulopolles@gmail.com>
2024-07-18 22:08:03 -0300

    diversas melhorias

--- app/Http/Controllers/FIDERatingController.php
+++ app/Http/Controllers/FIDERatingController.php
@@ -5,6 +5,15 @@ namespace App\Http\Controllers;
 use App\Enxadrista;
 use App\Http\Util\Util;
 use GuzzleHttp\Client;
+use Psr\Http\Client\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+
+
+use Symfony\Component\BrowserKit\HttpBrowser;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpClient\HttpClient;
 
 class FIDERatingController extends Controller
 {
@@ -121,105 +130,198 @@ class FIDERatingController extends Controller
     //     if($show_text) echo "<hr/>";
     // }
 
-    public static function getRating($enxadrista, $show_text = true, $return_enxadrista = false, $save_rating = true){
+    // public static function getRating($enxadrista, $show_text = true, $return_enxadrista = false, $save_rating = true)
+    // {
+    //     $codigo_organizacao = 0;
+
+
+    //     if ($show_text) echo "Enxadrista #" . $enxadrista->id . " - " . $enxadrista->name . "(" . $enxadrista->fide_id . ")";
+
+    //     if (!env("FIDE_RATING_SERVER", false)) {
+    //         if ($save_rating) $enxadrista->deleteRating($codigo_organizacao, 0);
+    //         if ($save_rating) $enxadrista->deleteRating($codigo_organizacao, 1);
+    //         if ($save_rating) $enxadrista->deleteRating($codigo_organizacao, 2);
+
+
+    //         if ($save_rating) $enxadrista->fide_last_update = date("Y-m-d H:i:s");
+
+    //         if ($return_enxadrista) {
+    //             return $enxadrista;
+    //         } else {
+    //             $enxadrista->save();
+    //         }
+    //     } else {
+    //         $client = new Client([
+    //             'http_errors' => false,
+    //         ]);
+    //         $response = $client->get(env("FIDE_RATING_SERVER", false) . "/player/" . $enxadrista->fide_id . "/elo");
+    //         if ($show_text) echo "<br/>";
+
+    //         if ($response->getStatusCode() != 200) {
+    //             $html = "{}";
+    //         } else {
+    //             $html = (string) $response->getBody();
+    //         }
+
+    //         $not_found = true;
+
+    //         $json = json_decode($html);
+    //         if (!isset($json->reason)) {
+    //             $enxadrista->encontrado_fide = true;
+    //             if (!$return_enxadrista) $enxadrista->save();
+    //             $not_found = false;
+    //             if (isset($json->standard_elo)) {
+    //                 if (is_numeric($json->standard_elo)) {
+    //                     if ($show_text) echo "STD:" . $json->standard_elo;
+    //                     if ($save_rating) $enxadrista->setRating($codigo_organizacao, 0, intval($json->standard_elo));
+    //                 } else {
+    //                     if ($show_text) echo "STD: String (" . $json->standard_elo . ")";
+    //                     if ($save_rating) $enxadrista->deleteRating($codigo_organizacao, 0);
+    //                 }
+    //             } else {
+    //                 if ($show_text) echo "STD: Not Found";
+    //                 if ($save_rating) $enxadrista->deleteRating($codigo_organizacao, 0);
+    //             }
+    //             if ($show_text) echo "<br/>";
+    //             if (isset($json->rapid_elo)) {
+    //                 if (is_numeric($json->rapid_elo)) {
+    //                     if ($show_text) echo "RPD:" . $json->rapid_elo;
+    //                     if ($save_rating) $enxadrista->setRating($codigo_organizacao, 1, intval($json->rapid_elo));
+    //                 } else {
+    //                     if ($show_text) echo "RPD: String (" . $json->rapid_elo . ")";
+    //                     if ($save_rating) $enxadrista->deleteRating($codigo_organizacao, 1);
+    //                 }
+    //             } else {
+    //                 if ($show_text) echo "RPD: Not Found";
+    //                 if ($save_rating) $enxadrista->deleteRating($codigo_organizacao, 1);
+    //             }
+    //             if ($show_text) echo "<br/>";
+    //             if (isset($json->blitz_elo)) {
+    //                 if (is_numeric($json->blitz_elo)) {
+    //                     if ($show_text) echo "BTZ:" . $json->blitz_elo;
+    //                     if ($save_rating) $enxadrista->setRating($codigo_organizacao, 2, intval($json->blitz_elo));
+    //                 } else {
+    //                     if ($show_text) echo "BTZ: String (" . $json->blitz_elo . ")";
+    //                     if ($save_rating) $enxadrista->deleteRating($codigo_organizacao, 2);
+    //                 }
+    //             } else {
+    //                 if ($show_text) echo "BTZ: Not Found";
+    //                 if ($save_rating) $enxadrista->deleteRating($codigo_organizacao, 2);
+    //             }
+    //         } else {
+    //             $enxadrista->fide_name = null;
+    //             $enxadrista->encontrado_fide = false;
+    //             if (!$return_enxadrista) $enxadrista->save();
+    //         }
+
+
+
+    //         if ($not_found && $save_rating) {
+    //             $enxadrista->deleteRating($codigo_organizacao, 0);
+    //             $enxadrista->deleteRating($codigo_organizacao, 1);
+    //             $enxadrista->deleteRating($codigo_organizacao, 2);
+    //         }
+
+    //         if ($save_rating) $enxadrista->fide_last_update = date("Y-m-d H:i:s");
+    //         if ($return_enxadrista) {
+    //             return $enxadrista;
+    //         } else {
+    //             $enxadrista->save();
+    //         }
+    //     }
+    //     if ($show_text) echo "<hr/>";
+    // }
+    public static function getRating($enxadrista, $show_text = true, $return_enxadrista = false, $save_rating = true)
+    {
         $codigo_organizacao = 0;
 
 
-        if($show_text) echo "Enxadrista #" . $enxadrista->id . " - " . $enxadrista->name . "(".$enxadrista->fide_id.")";
+        $client = HttpClient::create();
+        $browser = new HttpBrowser($client);
 
-        if(!env("FIDE_RATING_SERVER",false)){
-            if($save_rating) $enxadrista->deleteRating($codigo_organizacao, 0);
-            if($save_rating) $enxadrista->deleteRating($codigo_organizacao, 1);
-            if($save_rating) $enxadrista->deleteRating($codigo_organizacao, 2);
+        $url = "https://ratings.fide.com/profile/".$enxadrista->fide_id;
 
 
-            if($save_rating) $enxadrista->fide_last_update = date("Y-m-d H:i:s");
+        try {
+            $crawler = $browser->request('GET', $url);
+            $statusCode = $browser->getInternalResponse()->getStatusCode();
 
-            if($return_enxadrista){
+            if ($statusCode !== 200) {
+                return view('chess.error', ['error' => 'Erro ao acessar a página: código de status ' . $statusCode]);
+
+
+            }
+
+            $players = [];
+
+            $crawler->filter('.contentpaneopen')->each(function (Crawler $node) use ($save_rating, &$enxadrista, $codigo_organizacao, $show_text) {
+                $name = $node->filter('td[align="left"] a')->text();
+                $ratings = $node->filter('td[align="center"]')->each(function (Crawler $ratingNode) {
+                    return $ratingNode->text();
+                });
+
+                if($name) {
+                    $enxadrista->fide_name = $name;
+                    if ($ratings[0]) {
+                        if ($show_text) echo "STD:" . $ratings[0];
+                        if ($save_rating) $enxadrista->setRating($codigo_organizacao, 0, intval($ratings[0]));
+                    }else{
+                        if ($show_text) echo "STD: Not Found";
+                        if ($save_rating) $enxadrista->deleteRating($codigo_organizacao, 0);
+                    }
+                    if ($ratings[1]) {
+                        if ($show_text) echo "RPD:" . $ratings[1];
+                        if ($save_rating) $enxadrista->setRating($codigo_organizacao, 1, intval($ratings[1]));
+                    } else {
+                        if ($show_text) echo "RPD: Not Found";
+                        if ($save_rating) $enxadrista->deleteRating($codigo_organizacao, 1);
+                    }
+                    if ($ratings[2]) {
+                        if ($show_text) echo "BTZ:" . $ratings[2];
+                        if ($save_rating) $enxadrista->setRating($codigo_organizacao, 2, intval($ratings[2]));
+                    } else {
+                        if ($show_text) echo "BTZ: Not Found";
+                        if ($save_rating) $enxadrista->deleteRating($codigo_organizacao, 2);
+                    }
+                }else{
+                    if ($save_rating) $enxadrista->deleteRating($codigo_organizacao, 0);
+                    if ($save_rating) $enxadrista->deleteRating($codigo_organizacao, 1);
+                    if ($save_rating) $enxadrista->deleteRating($codigo_organizacao, 2);
+
+
+                    if ($save_rating) $enxadrista->fide_last_update = date("Y-m-d H:i:s");
+                }
+
+                $players[] = [
+                    'name' => $name,
+                    'standard' => $ratings[0] ?? 'N/A',
+                    'rapid' => $ratings[1] ?? 'N/A',
+                    'blitz' => $ratings[2] ?? 'N/A'
+                ];
+
+            });
+
+
+
+
+            if ($save_rating) $enxadrista->fide_last_update = date("Y-m-d H:i:s");
+            if ($return_enxadrista) {
                 return $enxadrista;
-            }else{
+            } else {
                 $enxadrista->save();
             }
-        }else{
-            $client = new Client([
-                'http_errors' => false,
-            ]);
-            $response = $client->get(env("FIDE_RATING_SERVER",false)."/player/" . $enxadrista->fide_id . "/elo");
-            if($show_text) echo "<br/>";
-
-            if($response->getStatusCode() != 200){
-                $html = "{}";
-            }else{
-                $html = (string) $response->getBody();
-            }
-
-            $not_found = true;
-
-            $json = json_decode($html);
-            if(!isset($json->reason)){
-                $enxadrista->encontrado_fide = true;
-                if(!$return_enxadrista) $enxadrista->save();
-                $not_found = false;
-                if(isset($json->standard_elo)){
-                    if(is_numeric($json->standard_elo)){
-                        if($show_text) echo "STD:" . $json->standard_elo;
-                        if($save_rating) $enxadrista->setRating($codigo_organizacao, 0, intval($json->standard_elo));
-                    }else{
-                        if($show_text) echo "STD: String (".$json->standard_elo.")";
-                        if($save_rating) $enxadrista->deleteRating($codigo_organizacao, 0);
-                    }
-                }else{
-                    if($show_text) echo "STD: Not Found";
-                    if($save_rating) $enxadrista->deleteRating($codigo_organizacao, 0);
-                }
-                if($show_text) echo "<br/>";
-                if(isset($json->rapid_elo)){
-                    if(is_numeric($json->rapid_elo)){
-                        if($show_text) echo "RPD:" . $json->rapid_elo;
-                        if($save_rating) $enxadrista->setRating($codigo_organizacao, 1, intval($json->rapid_elo));
-                    }else{
-                        if($show_text) echo "RPD: String (".$json->rapid_elo.")";
-                        if($save_rating) $enxadrista->deleteRating($codigo_organizacao, 1);
-                    }
-                }else{
-                    if($show_text) echo "RPD: Not Found";
-                    if($save_rating) $enxadrista->deleteRating($codigo_organizacao, 1);
-                }
-                if($show_text) echo "<br/>";
-                if(isset($json->blitz_elo)){
-                    if(is_numeric($json->blitz_elo)){
-                        if($show_text) echo "BTZ:" . $json->blitz_elo;
-                        if($save_rating) $enxadrista->setRating($codigo_organizacao, 2, intval($json->blitz_elo));
-                    }else{
-                        if($show_text) echo "BTZ: String (".$json->blitz_elo.")";
-                        if($save_rating) $enxadrista->deleteRating($codigo_organizacao, 2);
-                    }
-                }else{
-                    if($show_text) echo "BTZ: Not Found";
-                    if($save_rating) $enxadrista->deleteRating($codigo_organizacao, 2);
-                }
-            }else{
-                $enxadrista->fide_name = null;
-                $enxadrista->encontrado_fide = false;
-                if(!$return_enxadrista) $enxadrista->save();
-            }
 
 
-
-            if($not_found && $save_rating){
-                $enxadrista->deleteRating($codigo_organizacao,0);
-                $enxadrista->deleteRating($codigo_organizacao,1);
-                $enxadrista->deleteRating($codigo_organizacao,2);
-            }
-
-            if($save_rating) $enxadrista->fide_last_update = date("Y-m-d H:i:s");
-            if($return_enxadrista){
-                return $enxadrista;
-            }else{
-                $enxadrista->save();
-            }
+            if ($save_rating) $enxadrista->fide_last_update = date("Y-m-d H:i:s");
+        } catch (TransportExceptionInterface | ClientExceptionInterface | ServerExceptionInterface | RedirectionExceptionInterface $e) {
+            return view('chess.error', ['error' => 'Erro ao acessar a página: ' . $e->getMessage()]);
         }
-        if($show_text) echo "<hr/>";
+
+
+        if ($show_text) echo "Enxadrista #" . $enxadrista->id . " - " . $enxadrista->name . "(" . $enxadrista->fide_id . ")";
+
+
+        if ($show_text) echo "<hr/>";
     }
 
     private static function getName($html){
