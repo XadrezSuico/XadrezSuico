25dcc38a650db12077225901d88c9b77211d4693
Joao <joaopaulopolles@gmail.com>
2024-08-02 23:57:08 -0300

    correção

--- resources/views/evento/edit.blade.php
+++ resources/views/evento/edit.blade.php
@@ -1614,8 +1614,8 @@
                                         <th>Período</th>
                                         @if(
                                             \Illuminate\Support\Facades\Auth::user()->hasPermissionGlobal() ||
-                                            \Illuminate\Support\Facades\Auth::user()->hasPermissionEventByPerfilByGroupEvent($grupo_evento->id,[4,5]) ||
-                                            \Illuminate\Support\Facades\Auth::user()->hasPermissionGroupEventByPerfil($grupo_evento->id,[7])
+                                            \Illuminate\Support\Facades\Auth::user()->hasPermissionEventByPerfilByGroupEvent($evento->grupo_evento->id,[4,5]) ||
+                                            \Illuminate\Support\Facades\Auth::user()->hasPermissionGroupEventByPerfil($evento->grupo_evento->id,[7])
                                         )
                                             <th>Status</th>
                                         @endif
