faff9a5d71c88dd6104193a0ccc6c5420ce00800
João Paulo Polles <joaopaulopolles@gmail.com>
2025-05-16 19:23:12 -0300

    correção

--- app/Exports/PREsporteTeamItemExport.php
+++ app/Exports/PREsporteTeamItemExport.php
@@ -87,7 +87,7 @@ class PREsporteTeamItemExport implements FromView, WithStyles, WithColumnWidths,
                     ],
                 ]);
 
-                $l = 8;
+                $l = 9;
                 foreach ($this->event->inscritosPorClube($this->club->id) as $id_categoria => $inscricoes) {
                     for ($a = "A"; $a < "L"; $a++) {
                         $cells_bottom[] = "{$a}{$l}";
