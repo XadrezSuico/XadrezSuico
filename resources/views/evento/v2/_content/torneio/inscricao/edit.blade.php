cc69e7c143104ae2d3def7e6c7bd04ce6b31434b
João Paulo Polles <joaopaulopolles@gmail.com>
2026-09-04 23:05:39 -0300

    ajuste

--- resources/views/evento/v2/_content/torneio/inscricao/edit.blade.php
+++ resources/views/evento/v2/_content/torneio/inscricao/edit.blade.php
@@ -1,3 +1,14 @@
+@php
+        $permitido_edicao = false;
+        if(
+            \Illuminate\Support\Facades\Auth::user()->hasPermissionGlobal() ||
+            \Illuminate\Support\Facades\Auth::user()->hasPermissionEventByPerfil($evento->id,[4]) ||
+			\Illuminate\Support\Facades\Auth::user()->hasPermissionGroupEventByPerfil($evento->grupo_evento->id,[7])
+        ){
+            $permitido_edicao = true;
+        }
+@endphp
+
 @if($permitido_edicao)
     <div class="modal fade modal-warning" id="novaCidade" tabindex="-1" role="dialog" aria-labelledby="alerts">
         <div class="modal-dialog" role="document">
